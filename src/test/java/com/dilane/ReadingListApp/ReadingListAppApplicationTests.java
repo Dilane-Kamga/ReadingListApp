05fb94523d7dd05b00ca294105ea2606169c491e
Dilane-Kamga <67242777+Dilane-Kamga@users.noreply.github.com>
2022-10-01 20:10:07 +0100

    Build a complete Reading List App

---
+++ src/test/java/com/dilane/ReadingListApp/ReadingListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilane.ReadingListApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
